fa1a67911859397674270f9fc95ecfce31f3cef0
soledemi <soledemi@yahoo.com.ar>
2023-11-08 10:56:46 -0300

    inicial

---
+++ src/main/java/com/argentinaprograma/Modelos/Alumno.java
@@ -0,0 +1,11 @@
+package com.argentinaprograma.Modelos;
+
+import lombok.Data;
+
+
+@Data
+public class Alumno {
+    private String nombre;
+    private String apellido;
+    
+}
---
+++ src/main/java/com/argentinaprograma/argentinaprograma40e2c138/Principal.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.argentinaprograma.argentinaprograma40e2c138;
+
+import com.argentinaprograma.Modelos.Alumno;
+import java.io.IOException;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Principal {
+    public static void main(String[] args){
+        Alumno alumno = new Alumno();
+        System.err.println(alumno.getNombre());
+    }
+}
